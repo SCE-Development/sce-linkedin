456f269f7ac0a86f739960a7228970ab5e28d7b8
jask1m <jkim94960@gmail.com>
2026-03-18 15:10:11 -0700

    setup express server and define schema models

---
+++ api/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+const PORT = 8081;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
